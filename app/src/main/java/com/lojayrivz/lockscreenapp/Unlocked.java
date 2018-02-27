94b9239a6577f4184c6ef988f0f18bf0d042b4d7
lojayrivz <lojayrivz@gmail.com>
2018-02-27 13:16:08 +0800

    UI

---
+++ app/src/main/java/com/lojayrivz/lockscreenapp/Unlocked.java
@@ -0,0 +1,13 @@
+package com.lojayrivz.lockscreenapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Unlocked extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unlocked);
+    }
+}
